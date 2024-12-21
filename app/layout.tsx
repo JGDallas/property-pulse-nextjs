5a7186b6ca13ef8709302b48edb156c07f4c212b
Josue Garza <josh.garza@gmail.com>
2024-12-21 13:51:31 -0600

    added files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import '@/assets/styles/globals.css';
 
+export const metadata = {
+    title: 'Property Pulse', 
+    keywords: 'Property, Real Estate, Buy, Sell, Rent, Invest, Property Pulse',
+    description: 'Find the perfect rental property, buy or sell your home, or invest in real estate with Property Pulse. We are your property marketplace for all things real estate.'
+};
+
 const MainLayout = ({ children }) => {
     return ( <html>
         <body>
