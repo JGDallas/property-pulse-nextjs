79288739cb6c7b11e520ce577ec92168a79a477b
Josue Garza <josh.garza@gmail.com>
2025-02-12 17:39:12 -0600

    working on updates

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,13 @@
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import PropertyDetails from "@/components/PropertyDetails";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";   
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
 
 interface PropertyPageProps {
+
+
     params: {
         id: string;
     };
@@ -8,10 +16,36 @@ interface PropertyPageProps {
     };
 }
 
-const PropertyPage = ({ params, searchParams }: PropertyPageProps) => {
+const PropertyPage = async ({ params, searchParams }: PropertyPageProps) => {
+    await connectDB();
+    const property = await Property.findById(params.id).lean();
+
     return (
         <div>
-            Property Page {params.id}
+            {property && !Array.isArray(property) ? (
+                <>
+                    <PropertyHeaderImage image={property.images?.[0]} />
+                    <section>
+                        <div className="container m-auto py-6 px-6">
+                            <Link
+                                href="/properties"
+                                className="text-blue-500 hover:text-blue-600 flex items-center"
+                            >
+                                <FaArrowLeft className='mr-2'/>Back to Properties
+                            </Link>
+                        </div>
+                        <section className="bg-blue-50">
+                            <div className="container m-auto py-10 px-6">
+                                <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+                                    <PropertyDetails property={property} />
+                                </div>
+                            </div>
+                        </section>
+                    </section>
+                </>
+            ) : (
+                <section>Property not found</section>
+            )}
             <div>
                 Search Parameters:
                 <ul>
