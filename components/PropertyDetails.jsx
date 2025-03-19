803dec0b0d14c150380302083b7ffb1bb04c1837
Josue Garza <josh.garza@gmail.com>
2025-03-18 23:50:41 -0500

    updated with Font Awesome and dynamic elements

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -1,4 +1,12 @@
-import { FaTimes } from 'react-icons/fa';
+import {
+  FaTimes,
+  FaBed,
+  FaBath,
+  FaRulerCombined,
+  FaCheck,
+  FaMapMarker,
+  FaMap,
+} from 'react-icons/fa';
 const PropertyDetails = ({ property }) => {
   return (
     <main>
@@ -6,7 +14,7 @@ const PropertyDetails = ({ property }) => {
         <div className="text-gray-500 mb-4">{property.type}</div>
         <h1 className="text-3xl font-bold mb-4">{property.name}</h1>
         <div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
-          <i className="fa-solid fa-location-dot text-lg text-orange-700 mr-2"></i>
+          <FaMapMarker className="text-orange-700 mt-1 mr-1" />
           <p className="text-orange-700">
             {property.location.street} {property.location.city},{' '}
             {property.location.state} {property.location.zipcode}
@@ -23,7 +31,7 @@ const PropertyDetails = ({ property }) => {
               {property.rates.nightly ? (
                 `$${property.rates.nightly.toLocaleString()}`
               ) : (
-                <FaTimes className="text-red-500" />
+                <FaTimes className="text-red-700" />
               )}
             </div>
           </div>
@@ -54,15 +62,16 @@ const PropertyDetails = ({ property }) => {
         <h3 className="text-lg font-bold mb-6">Description & Details</h3>
         <div className="flex justify-center gap-4 text-blue-500 mb-4 text-xl space-x-9">
           <p>
-            <i className="fa-solid fa-bed"></i> {property.beds}
+            <FaBed className="inline-block mr-2" /> {property.beds}{' '}
             <span className="hidden sm:inline">Beds</span>
           </p>
           <p>
-            <i className="fa-solid fa-bath"></i> {property.baths}
+            <FaBath className="inline-block mr-2" /> {property.baths}
+            {''}
             <span className="hidden sm:inline">Baths</span>
           </p>
           <p>
-            <i className="fa-solid fa-ruler-combined"></i>
+            <FaRulerCombined className="inline-block mr-2" />
             {property.square_feet}{' '}
             <span className="hidden sm:inline">sqft</span>
           </p>
@@ -76,58 +85,12 @@ const PropertyDetails = ({ property }) => {
         <h3 className="text-lg font-bold mb-6">Amenities</h3>
 
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none">
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i> Wifi
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Full
-            kitchen
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Washer &
-            Dryer
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Free
-            Parking
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Hot Tub
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>24/7
-            Security
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Wheelchair
-            Accessible
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Elevator
-            Access
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Dishwasher
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Gym/Fitness
-            Center
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Air
-            Conditioning
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>
-            Balcony/Patio
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Smart TV
-          </li>
-          <li>
-            <i className="fas fa-check text-green-600 mr-2 mt-3"></i>Coffee
-            Maker
-          </li>
+          {property.amenities.map((amenity, index) => (
+            <li key={index} className="flex items-center mb-4">
+              <FaCheck className="text-green-500 mr-2" />
+              {amenity}
+            </li>
+          ))}
         </ul>
       </div>
       {/* <!-- Map --> */}
